6ad4611f46662839ad106fccf7fc5ef7dd3574be
Arunkumar <arunkumar.nura18@gmail.com>
2026-08-05 11:00:36 +0530

    package installed

---
+++ src/component/InputBox.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const InputBox = ({
+  label,
+  amount,
+  onAmountChange,
+  onCurrencyChange,
+  currencyoption = [],
+  selectedCurrency = "usd",
+  amountDisabled = false,
+  currencyDisabled = false,
+  className = "",
+}) => {
+  return <div>InputBox</div>;
+};
+
+export default InputBox;
