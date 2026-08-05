52ceec9b36c5c2b16cc6a75fc54f1af76006ccb6
Arunkumar <arunkumar.nura18@gmail.com>
2026-08-05 20:26:27 +0530

    create an custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import { useState } from "react";
 import InputBox from "./component/InputBox";
+import useCurrencyInfo from "./hooks/useCurrencyInfo";
 
 function App() {
+  const [amount, setAmount] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+  const [convertedAmount, setConvertedAmount] = useState(0);
+
+  const currencyInfo = useCurrencyInfo(from);
   return (
     <>
       <div
@@ -9,8 +17,8 @@ function App() {
           backgroundImage: `url(https://images.pexels.com/photos/1144259/pexels-photo-1144259.jpeg)`,
         }}
       >
-        <InputBox />
-        <InputBox />
+        <InputBox label="From" amount={amount} />
+        <InputBox label="To" amount={amount} />
       </div>
     </>
   );
